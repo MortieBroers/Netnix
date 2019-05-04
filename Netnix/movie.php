6996cd51d453b93c5f894b6c6895dbe1e8072cab
NeetjeBroers <nathan.broers2000@gmail.com>
2019-05-04 17:42:36 +0200

    Update movie.php

--- Netnix/movie.php
+++ Netnix/movie.php
@@ -1,29 +1,26 @@
-<?php
-require_once "navbar.php";
-
-?>
+<?php require_once "navbar.php" ?>
 <div>
     <h2 class="w3-container w3-blue w3-center w3-allerta TextBar">Movies</h2>
+
     <?php
-
-    $loginToken = $_SESSION['loginToken'];
-
+    $select = $con->prepare("SELECT * FROM film");
+    $select->setFetchMode(PDO::FETCH_ASSOC);
+    $select->execute();
+    echo "<table border='1'>";
     while ($row = $select->fetch()) {
-        $query = "SELECT * FROM film";
-        $select = $con->prepare($query);
-        $select->setFetchMode(PDO::FETCH_ASSOC);
-        $select->execute();
-        $rowProfile = $select->fetch();
-        echo "<table class='w3-bordered w3-border indexStuff'>";
-        echo "<tr class='w3-border'>";
-        echo "  <th>Title</th>
-              </tr>";
-        echo "<tr class='w3-border'>";
-        echo "<td>" . $rowProfile['title'] . "</td>";
-        echo "</tr>";
-        echo "<br/>";
-        echo "</table>";
+        echo "  <tr>";
+
+        echo "    <td>";
+        echo $row['title'];
+        echo "    </td>";
+        echo "    <td>";
+        echo $row['description'];
+        echo "    </td>";
+        echo "    </td>";
+        echo "  </tr>";
     }
+    echo "</table>";
     ?>
+
 
 </div>
