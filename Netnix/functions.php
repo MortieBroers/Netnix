15c1aba6b3d57a51694bc8ef3aabff56c4c61171
NeetjeBroers <nathan.broers2000@gmail.com>
2019-05-04 17:54:31 +0200

    update functions.php

--- Netnix/functions.php
+++ Netnix/functions.php
@@ -5,13 +5,42 @@ function Redirect($url, $permanent = false)
     exit();
 }
 
-function CheckLoginToken(){    
-    if( isset( $_SESSION['loginToken'] ) ) 
-    {
-          $loginToken = $_SESSION['loginToken'];      
-    }
-    else 
-    { 
+function CheckLoginToken()
+{
+    if (isset($_SESSION['loginToken'])) {
+        $loginToken = $_SESSION['loginToken'];
+    } else {
         Redirect('login.php', false);
     }
+}
+
+function ShowAllMovies()
+{
+    require_once "config.1.php";
+    require_once "getprofile.php";
+
+    $select = $con->prepare("SELECT * FROM film");
+    $select->setFetchMode(PDO::FETCH_ASSOC);
+    $select->execute();
+    echo "<table border='1'>";
+    echo "<tr>";
+    echo "<th>";
+    echo "Title";
+    echo "</th>";
+    echo "<th>";
+    echo "Description";
+    echo "</th>";
+    while ($row = $select->fetch()) {
+        echo "</tr>";
+        echo "  <tr>";
+        echo "    <td>";
+        echo $row['title'];
+        echo "    </td>";
+        echo "    <td>";
+        echo $row['description'];
+        echo "    </td>";
+        echo "    </td>";
+        echo "  </tr>";
+    }
+    echo "</table>";
 }
--- Netnix/movie.php
+++ Netnix/movie.php
@@ -1,33 +1,8 @@
 <?php require_once "navbar.php" ?>
+<?php require_once "functions.php" ?>
 <div>
     <h2 class="w3-container w3-blue w3-center w3-allerta TextBar">Movies</h2>
-
-    <?php
-    $select = $con->prepare("SELECT * FROM film");
-    $select->setFetchMode(PDO::FETCH_ASSOC);
-    $select->execute();
-    echo "<table border='1'>";
-        echo"<tr>";
-        echo"<th>";
-        echo"Title";
-        echo"</th>";        
-        echo"<th>";
-        echo"Description";
-        echo"</th>";
-    while ($row = $select->fetch()) {
-        echo"</tr>";
-        echo "  <tr>";
-        echo "    <td>";
-        echo $row['title'];
-        echo "    </td>";
-        echo "    <td>";
-        echo $row['description'];
-        echo "    </td>";
-        echo "    </td>";
-        echo "  </tr>";
-    }
-    echo "</table>";
+    <?php    
+    ShowAllMovies();
     ?>
-
-
 </div>
